2bca6342e611529bd37b121ba5d9772648c3aeaf
Conor <cdamasterson@gmail.com>
2017-03-09 14:22:26 -0500

    Add explosive contact to projectile

--- Assets/ProjectileScript.cs
+++ Assets/ProjectileScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ProjectileScript : MonoBehaviour {
 
+	public float radius = 10.0F;
+	public float power = 200.0F;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,5 +15,19 @@ public class ProjectileScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnCollisionEnter (Collision col) {
+		Vector3 explosionPos = transform.position;
+		Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+		foreach (Collider hit in colliders) {
+			Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+			if (rb != null) {
+				Debug.Log ("Pop");
+				rb.AddExplosionForce (power, explosionPos, radius, 3.0F);
+			}
+
+		}
 	}
 }
