1269a83e26c6e7159d85dfbe6a2f6baea7a0ead3
Conor <cdamasterson@gmail.com>
2017-03-24 15:12:41 -0400

    Damageable Script

---
+++ Assets/Damagable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damagable : MonoBehaviour {
+
+	public float maxHealth;
+	public float currentHealth;
+
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (currentHealth <= 0) {
+			Destroy (gameObject);
+		}
+		
+	}
+
+	public void TakeDamage (float damageToTake) {
+		currentHealth -= damageToTake;
+	}
+}
