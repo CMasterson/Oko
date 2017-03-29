83a73f0155f651975771fed034d43907eca0d2cc
Conor <cdamasterson@gmail.com>
2017-03-29 12:59:19 -0400

    Basic enemy ranged attacks

---
+++ Assets/EnemyRangedAttack.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRangedAttack : MonoBehaviour {
+
+
+	public float attackDelay;
+	public GameObject projectilePrefab;
+	public float rangedAttackDamage;
+	public float projectileSpeed;
+
+	float attackTime;
+
+	// Use this for initialization
+	void Start () {
+		attackTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time >= attackTime) {
+			Attack ();
+		}
+	}
+
+	void Attack () {
+
+		GameObject projectile = Instantiate (projectilePrefab);
+
+		projectile.transform.position = transform.Find("ProjectileEmitter").transform.position;
+		projectile.GetComponent<Rigidbody> ().velocity = transform.forward * projectileSpeed;
+		projectile.GetComponent<ProjectileScript> ().damage = rangedAttackDamage;
+
+		attackTime = Time.time + attackDelay;
+
+	}
+}
---
+++ Assets/FaceGameObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceGameObject : MonoBehaviour {
+
+
+	public GameObject targetGameObject;
+	public float damping;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var lookPos = targetGameObject.transform.position - transform.position;
+		lookPos.y = 0;
+		var rotation = Quaternion.LookRotation(lookPos);
+		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping); 
+	}
+}
