33866fcfa29aff86e1cd273a9cedaa55f989a515
Conor <cdamasterson@gmail.com>
2017-03-31 12:34:37 -0400

    Allow option to only face a game object with line of sight and/or maximum range

--- Assets/FaceGameObject.cs
+++ Assets/FaceGameObject.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class FaceGameObject : MonoBehaviour {
 
-
+	public bool requiresLineOfSight = false;
+	public float maxTargetAquisitionRange = 1000.0f;
 	public GameObject targetGameObject;
 	public float damping;
 
@@ -15,9 +16,30 @@ public class FaceGameObject : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
+		if (targetGameObject) {
+			
+			if (requiresLineOfSight) {
+				Vector3 direction = targetGameObject.transform.position - transform.position;
+				RaycastHit hit;	
+				if (Physics.Raycast (transform.position, direction, out hit, maxTargetAquisitionRange)) {
+					if (hit.transform == targetGameObject.transform) {
+						Turn ();
+					}
+				}
+			} else {
+				Turn ();
+			}
+		}
+	}
+
+	void Turn () {
+		
 		var lookPos = targetGameObject.transform.position - transform.position;
 		lookPos.y = 0;
 		var rotation = Quaternion.LookRotation(lookPos);
 		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping); 
 	}
+
+
 }
